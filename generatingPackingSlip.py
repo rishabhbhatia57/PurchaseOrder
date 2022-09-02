7a4674c97d187a14727fd61d88d8951bb97ecfee
Rishabh <rishabhbhatia57@gmail.com>
2022-09-02 14:37:25 +0530

    Changes

--- generatingPackingSlip.py
+++ generatingPackingSlip.py
@@ -9,7 +9,7 @@ import shutil
 
 
 
-def generatingPackingSlip(PivotInput,templateSource,DestinationFolder):
+def generatingPackingSlip():
     startedTemplating = time.time()
     sourcePivot = "./Week/PivotTable/PivotTableoutput.xlsx"
     source = "./Week/PackingSlip-Template.xlsx"
@@ -39,7 +39,7 @@ def generatingPackingSlip(PivotInput,templateSource,DestinationFolder):
     # Logic for adding Data from Pivot file to Packing Slip template
     
     
-    for column in range(2,cols+1):
+    for column in range(2,cols-3):
         startedTemplatingFile = time.time()
         # Making Copy of template file
         shutil.copy(source, destination)
@@ -49,27 +49,29 @@ def generatingPackingSlip(PivotInput,templateSource,DestinationFolder):
         TemplateWorkbook = load_workbook(destination)
         TemplateSheet = TemplateWorkbook['ORDER']
 
-        TemplateSheet.cell(3,1).value = InputSheet.cell(1,2).value
-
-        
+        TemplateSheet.cell(3,1).value = InputSheet.cell(3,2).value # Vendor Name
+        TemplateSheet.cell(6,4).value = InputSheet.cell(7,2).value # Order Name
+        TemplateSheet.cell(5,1).value = InputSheet.cell(7,2).value # Order Name
         # PO Number
-        filename = InputSheet.cell(2,column).value
-        TemplateSheet.cell(4,2).value = InputSheet.cell(2,column).value
-        TemplateSheet.cell(5,1).value = InputSheet.cell(2,column).value
-        TemplateSheet.cell(6,1).value = InputSheet.cell(2,column).value
-        TemplateSheet.cell(6,2).value = InputSheet.cell(2,column).value
-        TemplateSheet.cell(6,3).value = InputSheet.cell(2,column).value
-        TemplateSheet.cell(6,4).value = InputSheet.cell(2,column).value
-
+        filename = InputSheet.cell(4,column).value
+        TemplateSheet.cell(5,2).value = InputSheet.cell(4,column).value
+        TemplateSheet.cell(6,2).value = InputSheet.cell(4,column).value
+        TemplateSheet.cell(6,1).value = InputSheet.cell(4,column).value
+        TemplateSheet.cell(6,3).value = InputSheet.cell(4,column).value
         # Receving Location
-        TemplateSheet.cell(5,3).value = InputSheet.cell(3,column).value
+        TemplateSheet.cell(5,3).value = InputSheet.cell(5,column).value
+        TemplateSheet.cell(4,2).value = InputSheet.cell(5,column).value
+
+        # StyleName
+        TemplateSheet.cell(8,1).value = "=VLOOKUP(,'C:/Users/HP/Desktop/[Book1.xlsx]Sheet1'!$A$2:$D$22292,2,FALSE)"
+
         # Copy EAN to template sheet
 
         # column = 2
 
         Trows = 8
         Tcols = 5
-        for row in range(5,rows+1):
+        for row in range(7,rows):
             # if InputSheet.cell(row,column).value != None or InputSheet.cell(row,column).value != "":
             if str(InputSheet.cell(row,column).value).isnumeric():
                     
@@ -79,6 +81,13 @@ def generatingPackingSlip(PivotInput,templateSource,DestinationFolder):
                 
                 # Copy EAN to template sheet
                 TemplateSheet.cell(Trows,Tcols-3).value = InputSheet.cell(row,1).value
+
+                # Closing Stock
+                # TemplateSheet.cell(Trows,Tcols+5).value = InputSheet.cell(row,column+1).value
+
+
+                # VLOOKUP
+                
                 Trows += 1 
 
         
@@ -86,20 +95,7 @@ def generatingPackingSlip(PivotInput,templateSource,DestinationFolder):
 
         print("Packing template Generated for: "+str(filename)+ " file - {:.2f}".format(time.time() - startedTemplatingFile,2)+ " seconds.")
 
-        
-
-
-    # Saving files as Packing Slip - PO Number.xlsxwriter
-
-
-    # Move final file to Packing Slip folder/ Date wise Packing Skip genaration folder/
-
-
-    # Remove the intermediate template file
-
-
-    # Done
-
     print("Total time taken {:.2f}".format(time.time() - startedTemplating,2)+ " seconds.")
     return 'Completed!'
 
+generatingPackingSlip()
